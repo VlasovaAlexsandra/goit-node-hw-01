b52f17dac06f92878bbdbdfb6516b71bda221619
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-06-06 10:34:55 +0300

    error

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // listContacts().then(data => console.table(data))
-const argv = require('yargs').argv;
+// const argv = require('yargs').argv;
 
 const contactsAPI = require('./contacts')
+
+const { Command } = require('commander')
+
+const program = new Command()
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const {
     listContacts,
