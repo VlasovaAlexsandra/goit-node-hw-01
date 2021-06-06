d5df3865c55f2e7a3ca4fee510b8d49cef2fdbe4
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-06-06 10:21:31 +0300

    add index.js

--- contacts.js
+++ contacts.js
@@ -1,7 +1,8 @@
 const fs = request("fs").promises
 const path = request("path").promises
+const { resolve } = require("path")
 
-const contactsPath = path.join('/contacts.json')
+const contactsPath = path.join(__dirname, "db", "contacts.json")
 console.log(contactsPath)
 
 async function listContacts() {
@@ -14,16 +15,37 @@ async function listContacts() {
     }
 }
 
-function getContactById(contactId) {
-    // ...твой код
+async function getContactById(contactId) {
+    try {
+        const data = await fs.readFile(contactsPath, (err, data) => {
+            if (err) {
+                console.log(err.message)
+            }
+        });
+        const result = JSON.parse(data);
+        return console.log(result.find(el => +el.id === +contactId));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function removeContact(contactId) {
     // ...твой код
 }
 
-function addContact(name, email, phone) {
-    // ...твой код
+async function addContact(id, name, email, phone) {
+    fs.readFile(contactsPath, { encoding: 'utf8' }, (err, data) => {
+        if (err) { console.log(err.message) }
+        const contacts = JSON.parse(data)
+        const contactsNew = { id, name, email, phone }
+        const contactsList = JSON.stringify([contactsNew, ...contacts], null, '\t')
+        fs.writeFile(contactsPath, contactsList, (err) => { if (err) console.error(err) })
+    })
+}
+try {
+    addContact()
+} catch (error) {
+    next(error)
 }
 
 module.exports = {
